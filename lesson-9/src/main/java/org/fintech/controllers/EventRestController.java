a0d14303380b438dd10338a84a251898f6be6ed3
Sergey T <moolycooly2004@gmail.com>
2024-10-24 14:51:35 +0300

    Дз№9 добавил краткий сваггер док

--- lesson-9/src/main/java/org/fintech/controllers/EventRestController.java
+++ lesson-9/src/main/java/org/fintech/controllers/EventRestController.java
@@ -1,5 +1,11 @@
 package org.fintech.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,6 +15,8 @@ import org.fintech.controllers.payload.EventFilter;
 import org.fintech.exception.EventNotFoundException;
 import org.fintech.services.EventService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -19,8 +27,17 @@ import java.util.List;
 @RequiredArgsConstructor
 @Slf4j
 @RequestMapping("/event")
+@Tag(name="Events Controller")
 public class EventRestController {
     private final EventService eventService;
+
+    @Operation(
+            summary = "Get events by filter",
+            responses ={
+                    @ApiResponse(responseCode = "200", description = "Success",
+                            content = @Content(mediaType = "application/json", array=@ArraySchema(schema = @Schema(implementation = EventDto.class))))
+            }
+    )
 
     @GetMapping
     public List<EventDto> getEventsByFilters(EventFilter filter) {
@@ -30,20 +47,58 @@ public class EventRestController {
         return eventService.findAllEventsByFilter(filter.getName(),filter.getDescription(),filter.getFromDate(),filter.getToDate(), filter.getPlaceId());
 
     }
+    @Operation(
+            summary = "Get event by id",
+            responses ={
+                    @ApiResponse(responseCode = "200", description = "Success",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = EventDto.class))),
+                    @ApiResponse(responseCode = "404", description = "Event was not found",
+                            content = @Content(mediaType = MediaType.APPLICATION_PROBLEM_JSON_VALUE, schema = @Schema(implementation = ProblemDetail.class))),
+            }
+    )
     @GetMapping("/{id}")
     public EventDto getEventById(@PathVariable(name = "id") int id) {
         return eventService.findEventById(id).orElseThrow(() -> new EventNotFoundException(id));
     }
+    @Operation(
+            summary = "Create event",
+            responses ={
+                    @ApiResponse(responseCode = "200", description = "Success",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = EventDto.class))),
+                    @ApiResponse(responseCode = "404", description = "Place was not found",
+                            content = @Content(mediaType = MediaType.APPLICATION_PROBLEM_JSON_VALUE, schema = @Schema(implementation = ProblemDetail.class))),
+                    @ApiResponse(responseCode = "400", description = "Invalid payload",
+                            content = @Content(mediaType = MediaType.APPLICATION_PROBLEM_JSON_VALUE, schema = @Schema(implementation = ProblemDetail.class))),
+            }
+    )
     @PostMapping
     public ResponseEntity<EventDto> createEvent(@RequestBody @Valid NewEventPayload newEvent) {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(eventService.createEvent(newEvent.placeId(),newEvent.name(),newEvent.description(),newEvent.date()));
     }
+    @Operation(
+            summary = "Update event by id",
+            responses ={
+                    @ApiResponse(responseCode = "200", description = "Success",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = EventDto.class))),
+                    @ApiResponse(responseCode = "404", description = "Event was not found",
+                            content = @Content(mediaType = MediaType.APPLICATION_PROBLEM_JSON_VALUE, schema = @Schema(implementation = ProblemDetail.class)))
+            }
+    )
     @PutMapping("/{id}")
     public void updateEvent(@RequestBody @Valid UpdateEventPayload updateEvent, @PathVariable(name = "id") int id) {
         eventService.updateEvent(id, updateEvent.name(), updateEvent.description(), updateEvent.date());
     }
+    @Operation(
+            summary = "Delete event by id",
+            responses ={
+                    @ApiResponse(responseCode = "204", description = "Success",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = void.class))),
+                    @ApiResponse(responseCode = "404", description = "Event was not found",
+                            content = @Content(mediaType = MediaType.APPLICATION_PROBLEM_JSON_VALUE, schema = @Schema(implementation = ProblemDetail.class)))
+            }
+    )
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteEvent(@PathVariable(name = "id") int id) {
         eventService.deleteEventById(id);
--- lesson-9/src/main/java/org/fintech/controllers/PlaceRestController.java
+++ lesson-9/src/main/java/org/fintech/controllers/PlaceRestController.java
@@ -1,13 +1,22 @@
 package org.fintech.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.fintech.controllers.payload.NewPlacePayload;
 import org.fintech.controllers.payload.UpdatePlacePayload;
+
 import org.fintech.exception.PlaceNotFoundException;
 import org.fintech.dto.PlaceDto;
 import org.fintech.services.PlaceService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,30 +25,73 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/place")
+@Tag(name="Place Controller")
 public class PlaceRestController {
     private final PlaceService placeService;
+
+    @Operation(
+            summary = "Get all places",
+            responses ={
+                    @ApiResponse(responseCode = "200", description = "Success",
+                            content = @Content(mediaType = "application/json", array=@ArraySchema(schema = @Schema(implementation = PlaceDto.class))))
+            }
+    )
     @GetMapping
     public List<PlaceDto> getAllPlaces() {
         return placeService.findAllPlaces();
     }
+    @Operation(
+            summary = "Get place by id",
+            responses ={
+                    @ApiResponse(responseCode = "200", description = "Success",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = PlaceDto.class))),
+                    @ApiResponse(responseCode = "404", description = "Place was not found",
+                            content = @Content(mediaType = MediaType.APPLICATION_PROBLEM_JSON_VALUE, schema = @Schema(implementation = ProblemDetail.class)))
+            }
+    )
     @GetMapping("/{id}")
     public PlaceDto getPlaceById(@PathVariable(name = "id") int id) {
         return placeService.findPlaceById(id).orElseThrow(() -> new PlaceNotFoundException(id));
     }
+    @Operation(
+            summary = "Create place",
+            responses ={
+                    @ApiResponse(responseCode = "200", description = "Success",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = PlaceDto.class))),
+                    @ApiResponse(responseCode = "400", description = "Invalid payload",
+                            content = @Content(mediaType = MediaType.APPLICATION_PROBLEM_JSON_VALUE, schema = @Schema(implementation = ProblemDetail.class))),
+            }
+    )
     @PostMapping
-    public ResponseEntity<PlaceDto> createSeller(@RequestBody @Valid NewPlacePayload newPlace) {
+    public ResponseEntity<PlaceDto> createPlace(@RequestBody @Valid NewPlacePayload newPlace) {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(placeService.createPlace(newPlace.slug(), newPlace.name()));
     }
-
+    @Operation(
+            summary = "Update place by id",
+            responses ={
+                    @ApiResponse(responseCode = "200", description = "Success",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = void.class))),
+                    @ApiResponse(responseCode = "404", description = "Place was not found",
+                            content = @Content(mediaType = MediaType.APPLICATION_PROBLEM_JSON_VALUE, schema = @Schema(implementation = ProblemDetail.class)))
+            }
+    )
     @PutMapping("/{id}")
     public void updatePlace(@RequestBody @Valid UpdatePlacePayload updatePlace, @PathVariable(name = "id") int id) {
         placeService.updatePlace(id, updatePlace.slug(), updatePlace.name());
     }
-
+    @Operation(
+            summary = "Delete place by id",
+            responses ={
+                    @ApiResponse(responseCode = "204", description = "Success",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = void.class))),
+                    @ApiResponse(responseCode = "404", description = "Place was not found",
+                            content = @Content(mediaType = MediaType.APPLICATION_PROBLEM_JSON_VALUE, schema = @Schema(implementation = ProblemDetail.class)))
+            }
+    )
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteSeller(@PathVariable(name = "id") int id) {
+    public ResponseEntity<?> deletePlace(@PathVariable(name = "id") int id) {
         placeService.deletePlaceById(id);
         return ResponseEntity.noContent().build();
     }
